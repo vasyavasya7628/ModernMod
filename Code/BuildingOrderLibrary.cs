a023efdce8ded1f41eb709990b775597d0e69dc7
Vasily7628 <mineok44@gmail.com>
2023-08-20 16:33:31 +0700

    Фикс багов

--- Code/BuildingOrderLibrary.cs
+++ Code/BuildingOrderLibrary.cs
@@ -12,10 +12,10 @@ namespace ModernMod
     {
         private static void initBuildingOrder()
         {   
-            
-            //HUMAN
             var human = AssetManager.race_build_orders.get("human");
-            human.addUpgrade("5house_human");
+            //addUpgrade("house_human_5", 500, 500, 1000, true, 0);
+            human.addUpgrade("house_human_5", 0, 0, 50, 0, false, false, 0);
+                //HUMAN
             BuildOrderLibrary.b.requirements_types = List.Of<string>("3hall_human", "4house_human");
             human.addUpgrade("6house_human");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("3hall_human", "5house_human");
@@ -27,7 +27,7 @@ namespace ModernMod
             BuildOrderLibrary.b.requirements_types = List.Of<string>("5hall_human", "8house_human");
             human.addUpgrade("10house_human");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("5hall_human", "9house_human");
-            human.addUpgrade("2hall_human", pPop: 100, pBuildings: 20);
+            human.addUpgrade("hall_human_2", pPop: 100, pBuildings: 20);
             human.addUpgrade("3hall_human", pPop: 150, pBuildings: 20);
             human.addUpgrade("4hall_human", pPop: 200, pBuildings: 20);
             human.addUpgrade("5hall_human", pPop: 250, pBuildings: 20);
@@ -35,7 +35,7 @@ namespace ModernMod
             //ORCS
             
             var orc = AssetManager.race_build_orders.get("orc");
-            orc.addUpgrade("5house_orc");
+            orc.addUpgrade("house_orc_5");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("3hall_orc", "4house_orc");
             orc.addUpgrade("6house_orc");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("3hall_orc", "5house_orc");
@@ -47,7 +47,7 @@ namespace ModernMod
             BuildOrderLibrary.b.requirements_types = List.Of<string>("5hall_orc", "8house_orc");
             orc.addUpgrade("10house_orc");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("5hall_orc", "9house_orc");
-            orc.addUpgrade("2hall_orc", pPop: 100, pBuildings: 20);
+            orc.addUpgrade("hall_orc_2", pPop: 100, pBuildings: 20);
             orc.addUpgrade("3hall_orc", pPop: 150, pBuildings: 20);
             orc.addUpgrade("4hall_orc", pPop: 200, pBuildings: 20);
             orc.addUpgrade("5hall_orc", pPop: 250, pBuildings: 20);
@@ -55,7 +55,7 @@ namespace ModernMod
             //ELF
             
             var elf = AssetManager.race_build_orders.get("elf");
-            elf.addUpgrade("5house_elf");
+            elf.addUpgrade("house_elf_5");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("3hall_elf", "4house_elf");
             elf.addUpgrade("6house_elf");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("3hall_elf", "5house_elf");
@@ -67,7 +67,7 @@ namespace ModernMod
             BuildOrderLibrary.b.requirements_types = List.Of<string>("5hall_elf", "8house_elf");
             elf.addUpgrade("10house_elf");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("5hall_elf", "9house_elf");
-            elf.addUpgrade("2hall_elf", pPop: 100, pBuildings: 20);
+            elf.addUpgrade("hall_elf_2", pPop: 100, pBuildings: 20);
             elf.addUpgrade("3hall_elf", pPop: 150, pBuildings: 20);
             elf.addUpgrade("4hall_elf", pPop: 200, pBuildings: 20);
             elf.addUpgrade("5hall_elf", pPop: 250, pBuildings: 20);
@@ -75,7 +75,7 @@ namespace ModernMod
             //DWARF
             
             var dwarf = AssetManager.race_build_orders.get("dwarf");
-            dwarf.addUpgrade("5house_dwarf");
+            dwarf.addUpgrade("house_dwarf_5");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("3hall_dwarf", "4house_dwarf");
             dwarf.addUpgrade("6house_dwarf");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("3hall_dwarf", "5house_dwarf");
@@ -87,7 +87,7 @@ namespace ModernMod
             BuildOrderLibrary.b.requirements_types = List.Of<string>("5hall_dwarf", "8house_dwarf");
             dwarf.addUpgrade("10house_dwarf");
             BuildOrderLibrary.b.requirements_types = List.Of<string>("5hall_dwarf", "9house_dwarf");
-            dwarf.addUpgrade("2hall_dwarf", pPop: 100, pBuildings: 20);
+            dwarf.addUpgrade("hall_dwarf_2", pPop: 100, pBuildings: 20);
             dwarf.addUpgrade("3hall_dwarf", pPop: 150, pBuildings: 20);
             dwarf.addUpgrade("4hall_dwarf", pPop: 200, pBuildings: 20);
             dwarf.addUpgrade("5hall_dwarf", pPop: 250, pBuildings: 20);
